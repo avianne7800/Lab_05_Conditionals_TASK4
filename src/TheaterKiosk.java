1c6d0c0921254bf2776802ce8020bf355040adff
avianne7800 <avianne7800@gmail.com>
2024-09-27 17:04:22 -0400

    doneeee

--- src/TheaterKiosk.java
+++ src/TheaterKiosk.java
@@ -1,5 +1,30 @@
-public class Main {
+import java.util.Scanner;
+public class TheaterKiosk {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        int Age = 0;
+        String Trash = "";
+        System.out.println("Enter your age.");
+
+        if(in.hasNextInt())
+        {
+            Age = in.nextInt();
+            in.nextLine();
+        }
+        else
+        {
+            Trash = in.nextLine();
+            System.out.println("You entered " + Trash + " which is invalid.");
+        }
+
+        if(Age >= 21)
+        {
+            in.nextLine();
+            System.out.println("Here is your wristband.");
+        }
+        else
+        {
+            System.out.println("You are not old enough.");
+        }
     }
 }
